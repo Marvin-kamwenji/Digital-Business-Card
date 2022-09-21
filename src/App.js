5cfc3c62ff326fbc00d115913776033d36742d7b
Marvin-kamwenji <90758968+Marvin-kamwenji@users.noreply.github.com>
2022-09-21 17:16:49 +0300

    adding several stylings

--- src/App.js
+++ src/App.js
@@ -1,49 +1,45 @@
 import marvImage from './marvimage.jpg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function App() {
   return (
-  //   <div className="App container">
-  //       <div></div>
-  //       <img src={marvImage} className="AppProfileImage" alt="profImage" />
-  //       <h3 className="headerName">
-  //         Marvin Kamwenji Murage
-  //       </h3>
-  //       <p className="professionName">
-  //         Software Engineer and Quality Assurance
-  //       </p>
-  //       <p className="websiteName">
-  //         marvinkamwenji.website
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //   </div>
-  // );
 
-
-  <div class="card" >
+  <div class="card rounded-2" >
       <img class="card-img-top" src={marvImage} alt="Card cap" />
 
         <div class="card-body">
             <h5 class="card-title"> Marvin Kamwenji Murage</h5>
-            <p class="card-text">Software Engineer and Quality Assurance</p>
-            <p class="card-text-1">marvinkamwenji.website</p>
+            <p class="card-subtitle">Software Engineer and Quality Assurance</p>
+            <p class="card-text">marvinkamwenji.website</p>
         </div>
-      <ul class="list-group list-group-flush">
-          <li class="list-group-item">Cras justo odio</li>
-          <li class="list-group-item">Dapibus ac facilisis in</li>
-          <li class="list-group-item">Vestibulum at eros</li>
-      </ul>
+       <div class="card-body">
+        <div className="buttons">
+          <button className="emailButton rounded-2"><FontAwesomeIcon icon="fa-regular fa-envelope" />Email</button>
+          <button className="linkedInButton rounded-2"><i class="fab fa-twitter fa-lg"></i>LinkedIn</button>
+        </div>
+        
+        <div>
+          <h5 className="aboutme">About</h5>
+          <p className="paragraphs">I am a highly motivated and passionate individual with skills and expertise in software development and Quality Assurance. 
+            I care deeply about building high quality softwares and I'm excited to put my creativity and skills to 
+            work to actively contribute towards creating innovative solutions. .</p>
+        </div>
 
-      <div class="card-body">
-          <a href="#" class="card-link">Card link</a>
-          <a href="#" class="card-link">Another link</a>
+        <div>
+          <h5 className="aboutme">Interests</h5>
+          <p className="paragraphs">I have interests in robotics, artificial intelligence and blockchain. </p>
+        </div>
+
+      </div>
+
+      <div className="card-footer">
+
+        {/* Twitter  */}
+        <i class="fab fa-facebook-f fa-lg"></i>
+
+        <i class="bi bi-instagram"></i>
+
       </div>
 </div>
   );
